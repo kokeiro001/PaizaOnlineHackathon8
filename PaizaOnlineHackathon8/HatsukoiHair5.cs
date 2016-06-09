6a760db7770712abfc4f4d035b3492f1e7bf5c10
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-10 08:44:44 +0900

    HatsukoiHair5の解答を別ファイルに移動した

---
+++ PaizaOnlineHackathon8/HatsukoiHair5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace PaizaOnlineHackathon8 {
+  public class HatsukoiHair5 : PaizaOnlineHackathon {
+
+    private int s;
+    private int t;
+    private string result;
+
+    protected override void ReadInput() {
+      s = int.Parse(Console.ReadLine());
+      t = int.Parse(Console.ReadLine());
+    }
+
+    protected override void Calclate() {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 1; i <= s; i++) {
+        sb.Append(i == t ? "+" : "-");
+      }
+      result = sb.ToString();
+    }
+
+    protected override void Output() {
+      Console.WriteLine(result);
+    }
+  }
+}
--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -22,27 +22,4 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
-  public class HatsukoiHair5 : PaizaOnlineHackathon {
-
-    private int s;
-    private int t;
-    private string result;
-
-    protected override void ReadInput() {
-      s = int.Parse(Console.ReadLine());
-      t = int.Parse(Console.ReadLine());
-    }
-
-    protected override void Calclate() {
-      StringBuilder sb = new StringBuilder();
-      for (int i = 1; i <= s; i++) {
-        sb.Append(i == t ? "+" : "-");
-      }
-      result = sb.ToString();
-    }
-
-    protected override void Output() {
-      Console.WriteLine(result);
-    }
-  }
 }
