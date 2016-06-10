aad4af603bbc2109d5b93f64ed72ad320d06957f
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-10 09:26:25 +0900

    HatsukoiEye2を解くクラスを別ファイルに移動した

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -22,24 +22,4 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
-  public class HatsukoiEye2 : PaizaOnlineHackathon {
-
-    private int nokoriZasekiNum;
-    private int groupNum;
-    private string result;
-
-    protected override void ReadInput() {
-      int[] inputs = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
-      nokoriZasekiNum = inputs[0];
-      groupNum = inputs[1];
-    }
-
-    protected override void Calclate() {
-      result = nokoriZasekiNum >= groupNum ? "OK" : "NG";
-    }
-
-    protected override void Output() {
-      Console.WriteLine(result);
-    }
-  }
 }
