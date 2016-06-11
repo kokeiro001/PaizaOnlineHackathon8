27c484b8a8549f031c993eb44cd88b7d0c0c5c43
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 09:14:07 +0900

    解答を別ファイルに移動した

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -22,22 +22,4 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
-  public class HatsukoiEye3 : PaizaOnlineHackathon {
-
-    private int input;
-    private string result;
-
-    protected override void ReadInput() {
-      input = int.Parse(Console.ReadLine());
-    }
-
-    protected override void Calclate() {
-      int point = (input / 100) + ((input >= 1000) ? 10 : 0);
-      result = point.ToString();
-    }
-
-    protected override void Output() {
-      Console.WriteLine(result);
-    }
-  }
 }
