cefb56a1662c5e817733d7380a4f0fc416758dc4
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 11:23:04 +0900

    解いた

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -25,16 +25,35 @@ namespace PaizaOnlineHackathon8 {
 
   public class HatsukoiSpecial6 : PaizaOnlineHackathon {
 
+    private int funNum;
+    private int paperCost;
+    private int writablePaperCount;
+    private int penCost;
+    private string result;
+
     protected override void ReadInput() {
-      throw new NotImplementedException();
+      var line1 = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
+      funNum = line1[0];
+      paperCost = line1[1];
+
+      var line2 = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
+      writablePaperCount = line2[0];
+      penCost = line2[1];
     }
 
     protected override void Calclate() {
-      throw new NotImplementedException();
+      int totalPaperCost = funNum * paperCost;
+      int totalPenNum = (funNum / writablePaperCount);
+      if (funNum % writablePaperCount != 0) {
+        totalPenNum++;
+      }
+      int totalPenCost = totalPenNum * penCost;
+
+      result = (totalPaperCost + totalPenCost).ToString();
     }
 
     protected override void Output() {
-      throw new NotImplementedException();
+      Console.WriteLine(result);
     }
   }
 }
