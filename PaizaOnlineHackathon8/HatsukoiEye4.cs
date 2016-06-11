305755d15a8082282e73aba3ac6999887a21555e
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 09:27:01 +0900

    解答を別ファイルに移動した

---
+++ PaizaOnlineHackathon8/HatsukoiEye4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaizaOnlineHackathon8 {
+
+  public class HatsukoiEye4 : PaizaOnlineHackathon {
+
+    private List<int> numbers = new List<int>();
+    string result;
+
+    protected override void ReadInput() {
+      int n = int.Parse(Console.ReadLine());
+      numbers = Console.ReadLine().Split(' ').Select(numStr => int.Parse(numStr)).ToList();
+    }
+
+    protected override void Calclate() {
+      numbers.Sort();
+      result = numbers[numbers.Count / 2].ToString();
+    }
+
+    protected override void Output() {
+      Console.WriteLine(result);
+    }
+  }
+}
--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -23,23 +23,4 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
-  public class HatsukoiEye4 : PaizaOnlineHackathon {
-
-    private List<int> numbers = new List<int>();
-    string result;
-
-    protected override void ReadInput() {
-      int n = int.Parse(Console.ReadLine());
-      numbers = Console.ReadLine().Split(' ').Select(numStr => int.Parse(numStr)).ToList();
-    }
-
-    protected override void Calclate() {
-      numbers.Sort();
-      result = numbers[numbers.Count / 2].ToString();
-    }
-
-    protected override void Output() {
-      Console.WriteLine(result);
-    }
-  }
 }
