38cf7106c1ce059e0da5731721bbdb438a42bce1
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 11:38:51 +0900

    TestHelperを削除した

--- UnitTestProject/HatsukoiHair3Test.cs
+++ UnitTestProject/HatsukoiHair3Test.cs
@@ -6,34 +6,19 @@ using PaizaOnlineHackathon8;
 
 namespace UnitTestProject {
   [TestClass]
-  public class HatsukoiHair3Test {
-
-    private HatsukoiHair3 obj;
-
-    [TestInitialize]
-    public void Initialize() {
-      obj = new HatsukoiHair3();
-    }
+  public class HatsukoiHair3Test : POHTest<HatsukoiHair3> {
 
     [TestMethod]
     public void SampleInut1Test() {
-      StringBuilder input = new StringBuilder();
-      input.AppendLine("7");
-
-      StringBuilder expected = new StringBuilder();
-      expected.AppendLine("lucky");
-
-      TestHelper.Test(obj, input, expected);
+      Input.AppendLine("7");
+      Expected.AppendLine("lucky");
+      Test();
     }
     [TestMethod]
     public void SampleInut2Test() {
-      StringBuilder input = new StringBuilder();
-      input.AppendLine("12");
-
-      StringBuilder expected = new StringBuilder();
-      expected.AppendLine("unlucky");
-
-      TestHelper.Test(obj, input, expected);
+      Input.AppendLine("12");
+      Expected.AppendLine("unlucky");
+      Test();
     }
   }
 }
