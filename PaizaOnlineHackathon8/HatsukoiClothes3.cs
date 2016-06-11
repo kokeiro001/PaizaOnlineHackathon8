62ba482e00976cb14d03588ae22d599660494285
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 09:57:39 +0900

    解答を別ファイルに移動した

---
+++ PaizaOnlineHackathon8/HatsukoiClothes3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace PaizaOnlineHackathon8 {
+
+  public class HatsukoiClothes3 : PaizaOnlineHackathon {
+
+    private int forward;
+    private int back;
+    private string result;
+
+    protected override void ReadInput() {
+      var inputs = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
+      forward = inputs[0];
+      back = inputs[1];
+    }
+
+    protected override void Calclate() {
+      result = Math.Max(0, forward - back).ToString();
+    }
+
+    protected override void Output() {
+      Console.WriteLine(result);
+    }
+  }
+}
