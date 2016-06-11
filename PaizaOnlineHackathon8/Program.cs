2b4549abc84ce5962a324cc5614482d9dab14d6d
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 11:01:46 +0900

    テストを作成した

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace PaizaOnlineHackathon8 {
   class Program {
     static void Main(string[] args) {
-      PaizaOnlineHackathon obj = new HatsukoiSpecial5();
+      PaizaOnlineHackathon obj = new HatsukoiSpecial6();
       obj.Run();
     }
   }
@@ -23,4 +23,18 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
+  public class HatsukoiSpecial6 : PaizaOnlineHackathon {
+
+    protected override void ReadInput() {
+      throw new NotImplementedException();
+    }
+
+    protected override void Calclate() {
+      throw new NotImplementedException();
+    }
+
+    protected override void Output() {
+      throw new NotImplementedException();
+    }
+  }
 }
