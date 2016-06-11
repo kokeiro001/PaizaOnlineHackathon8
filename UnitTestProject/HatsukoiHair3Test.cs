b8ef739b2fc25d9d59543c32b0586bf0163c4025
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 11:42:25 +0900

    使用していないusingを削除した

--- UnitTestProject/HatsukoiHair3Test.cs
+++ UnitTestProject/HatsukoiHair3Test.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaizaOnlineHackathon8;
 
 namespace UnitTestProject {
