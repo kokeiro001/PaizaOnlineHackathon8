791c9d65d45af201e632b3cac27cb4abc6f283db
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-11 09:43:45 +0900

    解答を別ファイルに移動した

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -23,24 +23,4 @@ namespace PaizaOnlineHackathon8 {
     }
   }
 
-  public class HatsukoiClothes2 : PaizaOnlineHackathon {
-
-    private int staffNum;
-    private int candyNum;
-    private string result;
-
-    protected override void ReadInput() {
-      var inputs = Console.ReadLine().Split(' ').Select(str => int.Parse(str)).ToArray();
-      staffNum = inputs[0];
-      candyNum = inputs[1];
-    }
-
-    protected override void Calclate() {
-      result = (candyNum % staffNum == 0) ? "ok" : "ng";
-    }
-
-    protected override void Output() {
-      Console.WriteLine(result);
-    }
-  }
 }
