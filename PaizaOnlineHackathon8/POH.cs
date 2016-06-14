2a4d11fbf49483c9a4b7f94ef695b69afc6a833c
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-15 00:04:00 +0900

    リファクタリング

---
+++ PaizaOnlineHackathon8/POH.cs
@@ -0,0 +1,14 @@
+﻿namespace PaizaOnlineHackathon8 {
+
+  public abstract class PaizaOnlineHackathon {
+    protected abstract void ReadInput();
+    protected abstract void Calclate();
+    protected abstract void Output();
+
+    public void Run() {
+      ReadInput();
+      Calclate();
+      Output();
+    }
+  }
+}
--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -6,21 +6,9 @@ using System.Linq;
 namespace PaizaOnlineHackathon8 {
   class Program {
     static void Main(string[] args) {
-      PaizaOnlineHackathon obj = new HatsukoiSpecial6();
-      obj.Run();
     }
   }
 
-  public abstract class PaizaOnlineHackathon {
-    protected abstract void ReadInput();
-    protected abstract void Calclate();
-    protected abstract void Output();
 
-    public void Run() {
-      ReadInput();
-      Calclate();
-      Output();
-    }
-  }
 
 }
