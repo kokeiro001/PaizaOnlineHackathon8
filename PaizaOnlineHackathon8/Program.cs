da8c63b55eb237ff410c9d0c5a1bc0e4144b9f0a
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-15 00:04:37 +0900

    HatsukoiHairをワンライナーで解いた

--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-
+using C = System.Console;
+using E = System.Linq.Enumerable;
 namespace PaizaOnlineHackathon8 {
   class Program {
     static void Main(string[] args) {
+      new HatsukoiHair2_One().Run();
     }
   }
 
+  public interface IOnePaizaOnlineHackathon {
+    void Run();
+  }
 
-
+  public class HatsukoiHair2_One : IOnePaizaOnlineHackathon {
+    public void Run() {
+      C.WriteLine(string.Join("\r\n", E.Range(0, int.Parse(C.ReadLine())).Select(_ => "t")).Replace("t", C.ReadLine()));
+    }
+  }
 }
