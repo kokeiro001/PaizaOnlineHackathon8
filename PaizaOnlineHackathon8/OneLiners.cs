5ef652c0f062f0ff546f35b839c087cd731bad80
kokeiro <kokeiro.ssbm.001@gmail.com>
2016-06-15 00:46:52 +0900

    HatsukoiHair5をワンライナーで解いた

--- PaizaOnlineHackathon8/OneLiners.cs
+++ PaizaOnlineHackathon8/OneLiners.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using C = System.Console;
 using E = System.Linq.Enumerable;
@@ -22,6 +23,12 @@ namespace PaizaOnlineHackathon8 {
   public class HatsukoiHair4_One : IOnePaizaOnlineHackathon {
     public void Run() {
       C.WriteLine(E.Range(0, 5).Select(_ => C.ReadLine().Split(' ')).Count(list => list[0] == list[1]) >= 3 ? "OK" : "NG");
+    }
+  }
+
+  public class HatsukoiHair5_One : IOnePaizaOnlineHackathon {
+    public void Run() {
+      C.WriteLine(Regex.Replace(string.Join("", E.Range(0, int.Parse(C.ReadLine())).Select(_ => "-")), "^(" + string.Join("", E.Range(0, int.Parse(C.ReadLine()) - 1).Select(_ => "-")) + ")-", "$1+"));
     }
   }
 }
--- PaizaOnlineHackathon8/Program.cs
+++ PaizaOnlineHackathon8/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Linq;
 using C = System.Console;
 using E = System.Linq.Enumerable;
@@ -9,11 +10,12 @@ using E = System.Linq.Enumerable;
 namespace PaizaOnlineHackathon8 {
   class Program {
     static void Main(string[] args) {
-      new HatsukoiHair4_One().Run();
+      new HatsukoiHair5_One().Run();
     }
   }
 
   public interface IOnePaizaOnlineHackathon {
     void Run();
   }
+
 }
